5ee5a0a7a99d1a73202acd31fd423b4ff351f0f4
Kris Litman <duguy8@gmail.com>
2021-02-21 20:35:27 -0500

    User story 2 complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #trips
-  resources :trips, only: [:index]
+  resources :trips, only: [:index, :show]
 end
--- spec/features/trips/index_spec.rb
+++ spec/features/trips/index_spec.rb
@@ -21,4 +21,17 @@ RSpec.describe "When I visit the hiking trips index page" do
     expect(page).to have_content(@hike2.name)
     expect(page).to have_content(@hike3.name)
   end
+
+  it "Each trips name is a link to that hikes show page" do
+
+    visit trips_path
+
+    expect(page).to have_link(@hike1.name)
+    expect(page).to have_link(@hike2.name)
+    expect(page).to have_link(@hike3.name)
+
+    click_link(@hike1.name)
+
+    expect(current_path).to eq(trip_path(@hike1.id))
+  end
 end
